102aa5bf1766e89a1d472c8ded048551a5cab7d6
Robinho1997 <robin.kempen@gmx.de>
2023-05-16 08:00:06 +0200

    ShoppingList Component

---
+++ src/ShoppingList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ShoppingList() {
+    return (  
+        <ul>
+            <li>Brot</li>
+            <li>Wasser</li>
+            <li>Orange</li>
+            <li>Saft</li>
+            <li>Milch</li>
+        </ul>
+    );
+}
+
+export default ShoppingList;
